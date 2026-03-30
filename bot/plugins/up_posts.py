16641485b107f3f1fab2b43af1c83ad584c0073f
Im-Sourav02 <souravagarwal14092007@gmail.com>
2026-03-30 21:46:00 +0530

    HElloo

--- bot/plugins/up_posts.py
+++ bot/plugins/up_posts.py
@@ -34,8 +34,7 @@ from bot.core.func_utils import (
 LOGGER = logging.getLogger(__name__)
 
 # MongoDB setup
-DB_URI = ""
-mongo_client = AsyncIOMotorClient(DB_URI)
+mongo_client = AsyncIOMotorClient(Var.DB_URI)
 db = mongo_client['AutoAniOngoing']
 
 
